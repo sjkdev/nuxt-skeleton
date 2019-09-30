dfd667e0e684d84b07497eb306b113fe0c9a6224
seanjamesking <helloseanking@yahoo.com>
2019-09-30 22:14:47 +0100

    init repo commit

---
+++ components/NavBar.vue
@@ -0,0 +1,21 @@
+<!-- components/Navbar.vue -->
+
+<template>
+  <div class="container">
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">Portfolio</a>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
+          <nuxt-link class="navbar-item" to="/about">About</nuxt-link>
+          <nuxt-link class="navbar-item" to="/blog">Blog</nuxt-link>
+          <nuxt-link class="navbar-item" to="/portfolio">Portfolio</nuxt-link>
+          <nuxt-link class="navbar-item" to="/projects">Projects</nuxt-link>
+          <nuxt-link class="navbar-item" to="/contact">Contact</nuxt-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="container">
+    <div>
+      <Navbar/>
+      <app-logo/>
+      <h1 class="title">
+        dwanuxt
+      </h1>
+      <h2 class="subtitle">
+        Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  /* min-height: 100vh; */
+  display: flex;
+  /* justify-content: center;
+  align-items: center;
+  text-align: center; */
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
